fc1507982c1253687a1317fc2a406fdde1173fe1
Maeva108 <makuetchemaeva9@gmail.com>
2025-05-02 13:55:17 +0100

    Gestion des interface

--- archivage/urls.py
+++ archivage/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('documents/', views.liste_documents, name='liste_documents'),
+    path('', views.accueil, name='accueil'),  # Page d'accueil
+    path('documents/', views.liste_documents, name='documents'),  # Liste des documents
+    path('categories/', views.liste_categories, name='categories'),  # Liste des catégories
 ]
--- archivage/views.py
+++ archivage/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-from .models import Document
+from .models import Document, Categorie
+
+def accueil(request):
+    return render(request, 'archivage/accueil.html')
 
 def liste_documents(request):
     documents = Document.objects.all()
     return render(request, 'archivage/liste_documents.html', {'documents': documents})
+
+def liste_categories(request):
+    categories = Categorie.objects.all()
+    return render(request, 'archivage/liste_categories.html', {'categories': categories})
